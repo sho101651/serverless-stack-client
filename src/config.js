8bca913b8d39a38d3c69341bb41297d32d0ddc76
sho101651 <sho.junurairat@hotmail.com>
2021-02-20 16:48:36 +0700

    setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-upload-sho101651",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://6649izkc34.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ABlzna4Dd",
+    APP_CLIENT_ID: "3c6eclhacrjijch0eejvjir3ek",
+    IDENTITY_POOL_ID: "us-east-1:3f7d85bc-a378-48f3-b6a9-f3e4995f3404",
+  },
+};
+
+export default config;
